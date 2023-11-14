b61ba3c6a863526a75be8b17723dc2969b49dd60
Redi Lleshi <redilleshi52@gmail.com>
2023-11-14 12:27:27 -0500

    Updated Keyboard on the Animal Encounter Page

--- lib/wildlife.dart
+++ lib/wildlife.dart
@@ -30,99 +30,103 @@ class WildLifePage extends ConsumerWidget {
             sight: sight,
             controller: TextEditingController(text: sight.amount.toString())))
         .toList();
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            context.go('/suppliesExpendedPage');
-          },
-          icon: const Icon(Icons.keyboard_backspace),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              context.go('/suppliesExpendedPage');
+            },
+            icon: const Icon(Icons.keyboard_backspace),
+          ),
+          title: const Text('Wild Life'),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          actions: [
+            const Icon(Icons.receipt_long),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.more_vert),
+            )
+          ],
         ),
-        title: const Text('Wild Life'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        actions: [
-          const Icon(Icons.receipt_long),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.more_vert),
-          )
-        ],
-      ),
-      resizeToAvoidBottomInset: false,
-      body: Column(children: <Widget>[
-        Container(
-          padding: const EdgeInsets.all(10),
-          color: Colors.white,
-          child: const ListTile(
-            leading: Text(''),
-            title: Text('Name'),
-            trailing: Text(
-              'No',
-              style: TextStyle(fontSize: 14),
+        resizeToAvoidBottomInset: false,
+        body: Column(children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(10),
+            color: Colors.white,
+            child: const ListTile(
+              leading: Text(''),
+              title: Text('Name'),
+              trailing: Text(
+                'No',
+                style: TextStyle(fontSize: 14),
+              ),
             ),
           ),
-        ),
-        SizedBox(
-            height: 450,
-            child: Scrollbar(
-              child: ListView.separated(
-                itemCount: sightControllers.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                      onTap: () {},
+          SizedBox(
+              height: 450,
+              child: Scrollbar(
+                child: ListView.separated(
+                  itemCount: sightControllers.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                        onTap: () {},
 
-                      // This sets text color and icon color to red when list tile is disabled and
-                      // green when list tile is selected, otherwise sets it to black.
+                        // This sets text color and icon color to red when list tile is disabled and
+                        // green when list tile is selected, otherwise sets it to black.
 
-                      leading: Image(
-                          image: ResizeImage(
-                              AssetImage(sightControllers[index]
-                                      .sight
-                                      .localImagePath ??
-                                  ''),
-                              width: 200,
-                              height: 150)),
-                      title: Text(sightControllers[index].sight.name),
-                      trailing: SizedBox(
-                        width: 80,
-                        height: 40,
-                        child: TextFormField(
-                            textAlign: TextAlign.center,
-                            textAlignVertical: TextAlignVertical.top,
-                            controller: sightControllers[index].controller,
-                            keyboardType: TextInputType.number,
-                            decoration: const InputDecoration(
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(width: .5),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(width: .5),
-                              ),
-                            )),
-                      ));
-                },
-                separatorBuilder: (BuildContext context, int index) =>
-                    const Divider(height: 1),
-              ),
-            )),
-        const SizedBox(
-          height: 50, // <-- SEE HERE
-        ),
-        FilledButton(
-            onPressed: () {
-              for (int i = 0; i < sightControllers.length; i++) {
-                ref.read(patrolLogProvider.notifier).addOrUpdateWildLifeSight(
-                    WildLifeSight(
-                        name: sightControllers[i].sight.name,
-                        amount: int.parse(sightControllers[i].controller.text),
-                        localImagePath: sights[i].localImagePath,
-                        createdBy: Vault.userName ?? "-"));
-              }
+                        leading: Image(
+                            image: ResizeImage(
+                                AssetImage(sightControllers[index]
+                                        .sight
+                                        .localImagePath ??
+                                    ''),
+                                width: 200,
+                                height: 150)),
+                        title: Text(sightControllers[index].sight.name),
+                        trailing: SizedBox(
+                          width: 80,
+                          height: 40,
+                          child: TextFormField(
+                              textAlign: TextAlign.center,
+                              textAlignVertical: TextAlignVertical.top,
+                              controller: sightControllers[index].controller,
+                              keyboardType: TextInputType.number,
+                              decoration: const InputDecoration(
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(width: .5),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(width: .5),
+                                ),
+                              )),
+                        ));
+                  },
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const Divider(height: 1),
+                ),
+              )),
+          const SizedBox(
+            height: 50, // <-- SEE HERE
+          ),
+          FilledButton(
+              onPressed: () {
+                for (int i = 0; i < sightControllers.length; i++) {
+                  ref.read(patrolLogProvider.notifier).addOrUpdateWildLifeSight(
+                      WildLifeSight(
+                          name: sightControllers[i].sight.name,
+                          amount:
+                              int.parse(sightControllers[i].controller.text),
+                          localImagePath: sights[i].localImagePath,
+                          createdBy: Vault.userName ?? "-"));
+                }
 
-              context.go('/notesPage');
-            },
-            child: const Text('Next'))
-      ]),
+                context.go('/notesPage');
+              },
+              child: const Text('Next'))
+        ]),
+      ),
     );
   }
 }
